7153096d1aaa41d5b82c4ea350b9bc5e9e9f9571
Piet Brömmel <piet@control-f.io>
2025-04-17 18:44:14 +0200

    add logging using the context from fastmcp

--- echo_mcp_server_for_testing/main.py
+++ echo_mcp_server_for_testing/main.py
@@ -1,14 +1,15 @@
 import os
 
-from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp import Context, FastMCP
 
-mcp = FastMCP("Echo")
+mcp = FastMCP("Echo Server", log_level="ERROR")
 
 
 @mcp.tool()
-def echo_tool(message: str) -> str:
+async def echo_tool(message: str, ctx: Context) -> str:
     """Echo a message as a tool"""
     SECRET_KEY = os.getenv("SECRET_KEY", "No secret key found")
+    await ctx.info(f"Processing echo request for message: '{message}'")
     return f"Tool echo: {message}. The environment variable SECRET_KEY is: {SECRET_KEY}"
 
 
